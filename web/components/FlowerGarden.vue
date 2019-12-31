ab085b3da9dbe1cf6815fcfbb8530a866bcb571d
Matt Taylor <nickylogic@gmail.com>
2019-12-31 00:05:45 -0600

    Draw a polygon

--- web/components/FlowerGarden.vue
+++ web/components/FlowerGarden.vue
@@ -1,17 +1,39 @@
 <template>
-  <img src="daisy.png" height="250" width="250">
+  <svg v-bind:height="height" v-bind:width="width">
+    <polygon v-bind:points="polyPoints"/>
+  </svg>
 </template>
 
 <script>
+let points = [
+  [200,10],
+  [250,190],
+  [160,180]
+];
+let xsort = points.map((p)=>p[0]).sort();
+let ysort = points.map((p)=>p[1]).sort();
+let n = points.length;
+let xmax = xsort[n-1];
+let ymax = ysort[n-1];
+
 module.exports = {
   mounted () {
-    console.log('Hello World')
-  }
+    console.log('xmax = '+xmax);
+    console.log('ymax = '+ymax);
+  },
+  data: () => ({
+    width: xmax,
+    height: ymax,
+    polyPoints: points.map((p)=>p[0]+','+p[1]).join(' '),
+  })
 }
 </script>
 
 <style scoped>
   span {
     background-color: lightgray;
+  }
+  polygon {
+    fill:lime;stroke:purple;stroke-width:1
   }
 </style>
